2aa5c1e817901f98b55d4ac71f3afa2f4dcdf67d
kavya <kavyababuji@gmail.com>
2025-12-14 23:12:03 +0530

    removed vercel.json

--- server/config/db.js
+++ server/config/db.js
@@ -1,44 +1,16 @@
-// const { default: mongoose } = require("mongoose")
-// require('dotenv').config();
+const { default: mongoose } = require("mongoose")
+require('dotenv').config();
 
-// const connectDB= async()=>{
-//     try {
+const connectDB= async()=>{
+    try {
         
-//         await mongoose.connect(process.env.Mongo_URI)
-//         console.log("MongoDB connected successfully using .env");
+        await mongoose.connect(process.env.Mongo_URI)
+        console.log("MongoDB connected successfully using .env");
         
-//     } catch (error) {
-//         console.log('MongoDB connection error',error);
+    } catch (error) {
+        console.log('MongoDB connection error',error);
         
-//     }
-// }
-
-// module.exports={connectDB}
-
-const mongoose = require("mongoose");
-
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = {
-    conn: null,
-    promise: null,
-  };
+    }
 }
 
-const connectDB = async () => {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.Mongo_URI, {
-      bufferCommands: false,
-    });
-  }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
-};
-
-module.exports = { connectDB };
+module.exports={connectDB}
